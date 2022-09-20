f59ad8a0c767bcc8a3c3666b99b7365559714777
wlisses <wlissesfernando285@gmail.com>
2022-09-20 09:21:56 -0300

    verificando requisito n°4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('testa o componente <NotFound/>', () => {
+  it('testa se existe o Header correto na página', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen.getByRole(
+      'heading',
+      {
+        name: /Page requested not found/i,
+      },
+    );
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('testa se existe a propriedade Alt com valor correto', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen.getByRole(
+      'img',
+      {
+        alt: /Pikachu crying because the page requested was not found/i,
+      },
+    );
+
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
